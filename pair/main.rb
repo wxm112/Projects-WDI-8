fffbb58b4249eb933502aa3febb76eecc9c80562
王雪梅 <wxm112@me.com>
2015-01-20 08:23:26 +1100

    final version

--- pair/main.rb
+++ pair/main.rb
@@ -7,7 +7,7 @@ get '/' do
 @link_2 = 'write_test'
 @link_text1 = 'Yes'
 @link_text2 = 'No'
-  erb :two_buttoms
+  erb :two_buttons
 end
 
 get '/pass' do
@@ -16,7 +16,7 @@ get '/pass' do
 @link_2 = 'write_code'
 @link_text1 = 'Yes'
 @link_text2 = 'No'
-  erb :two_buttoms
+  erb :two_buttons
 end
 
 get '/refactor' do
@@ -25,35 +25,35 @@ get '/refactor' do
 @link_2 = 'new_feature'
 @link_text1 = 'Yes'
 @link_text2 = 'No'
-  erb :two_buttoms
+  erb :two_buttons
 end
 
 get '/write_code' do
 @text = "Write just enough code for the test to pass."
 @link_1 = 'pass'
 @link_text1 = 'Done'
-  erb :one_buttom
+  erb :one_button
 end
 
 get '/write_test' do
 @text = "Write a test."
 @link_1 = 'pass'
 @link_text1 = 'Done'
-  erb :one_buttom
+  erb :one_button
 end
 
 get '/do_refactor' do
 @text = "Refactor the code."
 @link_1 = 'pass'
 @link_text1 = 'Done'
-  erb :one_buttom
+  erb :one_button
 end
 
 get '/new_feature' do
 @text = "Select a new feature to implement."
 @link_1 = '/'
 @link_text1 = 'Continue'
-  erb :one_buttom
+  erb :one_button
 end
 
 
