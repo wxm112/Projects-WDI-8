6ca87e1e640d89e4c6f58914b9c0fbb1d6cb7e01
王雪梅 <wxm112@me.com>
2015-01-20 03:15:48 +1100

    merged view files into two files

--- pair/main.rb
+++ pair/main.rb
@@ -2,36 +2,74 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 get '/' do
-
-  erb :mainpage
+@text = "Do you have a test for that?"
+@link_1 = 'pass'
+@link_2 = 'write_test'
+@link_text1 = 'Yes'
+@link_text2 = 'No'
+  erb :two_buttoms
 end
 
 get '/pass' do
-
-  erb :pass
+@text = "Does the test pass?"
+@link_1 = 'refactor'
+@link_2 = 'write_code'
+@link_text1 = 'Yes'
+@link_text2 = 'No'
+  erb :two_buttoms
 end
 
 get '/refactor' do
-
-  erb :refactor
+@text = "Need a refactor?"
+@link_1 = 'do_refactor'
+@link_2 = 'new_feature'
+@link_text1 = 'Yes'
+@link_text2 = 'No'
+  erb :two_buttoms
 end
 
 get '/write_code' do
-
-  erb :write_code
+@text = "Write just enough code for the test to pass."
+@link_1 = 'pass'
+@link_text1 = 'Done'
+  erb :one_buttom
 end
 
 get '/write_test' do
-
-  erb :write_test
+@text = "Write a test."
+@link_1 = 'pass'
+@link_text1 = 'Done'
+  erb :one_buttom
 end
 
 get '/do_refactor' do
-
-  erb :do_refactor
+@text = "Refactor the code."
+@link_1 = 'pass'
+@link_text1 = 'Done'
+  erb :one_buttom
 end
 
 get '/new_feature' do
-
-  erb :new_feature
+@text = "Select a new feature to implement."
+@link_1 = '/'
+@link_text1 = 'Continue'
+  erb :one_buttom
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
